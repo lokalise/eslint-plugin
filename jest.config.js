f22a5066cbff37f8f365965c04270917c7a5f31d
Arturs Vonda <github@artursvonda.lv>
2023-02-24 11:20:52 +0200

    chore: align with frontend library template (#22)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 // to get timezone sync with server https://github.com/jest-community/vscode-jest/issues/153
 process.env.TZ = 'UTC';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
